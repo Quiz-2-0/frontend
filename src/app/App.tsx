9b611630359bd62d4896d28cac02266d4492b072
George051191 <trubacheff_91@mail.ru>
2023-06-19 22:00:18 +0300

    fonts added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { FC, useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import styled from 'styled-components';
 
-const App = () => (
-  <div>
-    Hello world !
-  </div>
-);
+/// потом сделаем переключение темы
+const Main = styled.main`
+  height: 100%;
+  width: 100%;
+  font-family: 'SFProDisplay';
+  display: flex;
+  flex-direction: column;
+`;
+
+const App = () => {
+  const location = useLocation();
+  return (
+    <Main style={{ fontFamily: 'SFProDisplay' }}>
+      Hello world !
+    </Main>
+  );
+};
 
 export default App;
