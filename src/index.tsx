e007c6021f26e43b4ba4356f5b2ce043280e0e46
George051191 <trubacheff_91@mail.ru>
2023-06-19 13:35:52 +0300

    base done

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import { createRoot } from 'react-dom/client';
 
-import App from './app/App';
 import { BrowserRouter } from 'react-router-dom';
+import App from './app/App';
 
 const rootDiv = document.getElementById('root');
 const rootNode = createRoot(rootDiv as Element);
@@ -12,7 +12,6 @@ rootNode.render(
   <React.StrictMode>
 
     <BrowserRouter>
-
 
       <App />
     </BrowserRouter>
