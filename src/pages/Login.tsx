737c5bfc8c4065162df5f130348e065085e81799
Tamara Frantsuzova <t.f.66625262@yandex.ru>
2023-06-20 10:04:42 +0300

    add styles in login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable ternary/no-unreachable */
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { NavLink } from 'react-router-dom';
@@ -5,17 +6,29 @@ import {
   SplitLayout,
   SplitCol,
   FormLayout,
+  Title,
+  Group,
+  Div,
+  SegmentedControl,
   FormItem,
   Input,
   Checkbox,
   Button,
 } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
-import { Icon12EyeSlashOutline } from '@vkontakte/icons';
+import { Icon12EyeSlashOutline, Icon16View } from '@vkontakte/icons';
+import '../ui-lib/logo/logo.css';
+import '../ui-lib/input/input.css';
+import '../ui-lib/text/text.css';
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const changePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
 
   const {
     register,
@@ -23,8 +36,19 @@ const Login: React.FC = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<FormData>();
+  console.log(errors);
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+  type FormValues = {
+    email: string;
+    password: string;
+  };
+
+  const onSubmit = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    console.log(e.target.value);
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.currentTarget;
     if (name === 'email') setEmail(value);
     if (name === 'password') setPassword(value);
@@ -32,38 +56,123 @@ const Login: React.FC = () => {
 
   return (
     <SplitLayout>
-      <SplitCol style={{ maxWidth: 292 }}>
-        <NavLink to='/'>Logo</NavLink>
-        <FormLayout style={{ maxWidth: 410 }}>
-          <FormItem htmlFor='email' top='Логин'>
+      <SplitCol style={{ maxWidth: 292, margin: '72px 0 0 189px' }}>
+        <NavLink className='logo' to='/'>
+          Logo
+        </NavLink>
+        <FormLayout
+          onSubmit={onSubmit}
+          style={{
+            maxWidth: '410px',
+            width: '100%',
+            minHeight: '510px',
+            marginTop: '135px',
+            padding: '32px',
+            border: '1px solid #C1C7CD',
+            borderRadius: '32px',
+            boxSizing: 'border-box',
+          }}>
+          <Title
+            level='1'
+            style={{
+              paddingBottom: '48px',
+              fontWeight: 600,
+              fontSize: '20px',
+              lineHeight: '24px',
+              textAlign: 'center',
+              letterSpacing: '0.38px',
+              color: '#21272A',
+            }}>
+            Вход в личный кабинет
+          </Title>
+
+          <Group style={{ padding: 0, marginBottom: '32px' }}>
+            <Div style={{ padding: 0 }}>
+              <SegmentedControl
+                style={{ padding: 0, background: 'none', border: 'none' }}
+                options={[
+                  {
+                    label: 'Сотрудник',
+                    value: 'user',
+                  },
+                  {
+                    label: 'Администратор',
+                    value: 'admin',
+                  },
+                ]} />
+            </Div>
+          </Group>
+
+          <FormItem
+            htmlFor='email'
+            top='Логин'
+            style={{
+              padding: 0,
+              marginBottom: '16px',
+              fontWeight: 400,
+              fontSize: '14px',
+              lineHeight: '140%',
+              color: '#21272A',
+            }}>
             <Input
               id='email'
               type='email'
               placeholder='Введите рабочую почту'
               name='email'
               value={email}
-              onChange={onChange} />
+              onChange={onChange}
+              className='input' />
           </FormItem>
-
-          <FormItem top='Пароль' htmlFor='pass'>
+          <FormItem
+            top='Пароль'
+            htmlFor='pass'
+            style={{ padding: 0, marginBottom: '16px' }}>
             <Input
               id='pass'
-              type='password'
+              type={isPasswordVisible ? 'text' : 'password'}
               placeholder='Введите пароль'
               name='password'
               value={password}
-              after={<Icon12EyeSlashOutline />}
-              onChange={onChange} />
+              after={
+                isPasswordVisible ? (
+                  <Icon16View
+                    width={24}
+                    height={24}
+                    onClick={changePasswordVisibility} />
+                ) : (
+                  <Icon12EyeSlashOutline
+                    width={24}
+                    height={24}
+                    onClick={changePasswordVisibility} />
+                )
+              }
+              onChange={onChange}
+              className='input' />
           </FormItem>
-          <Checkbox>Запомнить меня</Checkbox>
-          <FormItem>
-            <Button size='l' stretched>
+          <Div
+            style={{
+              padding: 0,
+              marginBottom: '16px',
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}>
+            <Checkbox className='text'>Запомнить меня</Checkbox>
+            <NavLink to='/reset-password' className='text'>
+              Сбросить пароль
+            </NavLink>
+          </Div>
+          <FormItem style={{ padding: 0 }}>
+            <Button
+              size='l'
+              style={{ background: '#5181B8', borderRadius: '4px' }}
+              stretched>
               Войти
             </Button>
           </FormItem>
         </FormLayout>
       </SplitCol>
-      <SplitCol />
+      <SplitCol style={{ background: '#5181B8' }} />
     </SplitLayout>
   );
 };
---
+++ src/vendor/index.tsx
@@ -0,0 +1,50 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+import BoldWoff from './fonts/SFProDisplay-Bold.woff';
+import BoldWoff2 from './fonts/SFProDisplay-Bold.woff2';
+import MediumWoff from './fonts/SFProDisplay-Medium.woff';
+import MediumWoff2 from './fonts/SFProDisplay-Medium.woff2';
+import RegularWoff from './fonts/SFProDisplay-Regular.woff';
+import RegularWoff2 from './fonts/SFProDisplay-Regular.woff2';
+import SemiboldWoff from './fonts/SFProDisplay-Semibold.woff';
+import SemiboldWoff2 from './fonts/SFProDisplay-Semibold.woff2';
+
+export const GlobalStyles = createGlobalStyle`
+  ${normalize}
+`;
+
+export const SFProDisplay = createGlobalStyle`
+    @font-face {
+        font-family: 'SFProDisplay';
+        src: local('SFProDisplay'),
+            url(${BoldWoff}) format('woff'),
+            url(${BoldWoff2}) format('woff2');
+            font-weight: 700;
+            font-style: normal;
+    }
+    @font-face {
+        font-family: 'SFProDisplay';
+        src: local('SFProDisplay'),
+            url(${SemiboldWoff}) format('woff'),
+            url(${SemiboldWoff2}) format('woff2');
+            font-weight: 600;
+            font-style: normal;
+    }
+    @font-face {
+        font-family: 'SFProDisplay';
+        src: local('SFProDisplay'),
+            url(${MediumWoff}) format('woff'),
+            url(${MediumWoff2}) format('woff2');
+            font-weight: 500;
+            font-style: normal
+    }
+    @font-face {
+        font-family: 'SFProDisplay';
+        src: local('SFProDisplay'),
+            url(${RegularWoff}) format('woff'),
+            url(${RegularWoff2}) format('woff2');
+            font-weight: 400;
+            font-style: normal
+    }
+`;
