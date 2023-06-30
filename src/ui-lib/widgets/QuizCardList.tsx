652a31153d116e3350ecb8b47ff909b775353aa9
Maria Arkhipova <marias1403@yandex.ru>
2023-06-30 19:30:46 +0900

    fix: QuizCard, QuizCardList

--- src/ui-lib/widgets/QuizCardList.tsx
+++ src/ui-lib/widgets/QuizCardList.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
 import QuizCard from './QuizCard';
+import { mockQuizes } from '../../constants/mock-data';
 
 const StyledQuizListContainer = styled.ul`
   margin: 0;
@@ -12,17 +13,26 @@ const StyledQuizListContainer = styled.ul`
   gap: 42px;
 `;
 
+interface Quiz {
+  image: string,
+  name: string;
+  description: string;
+  duration: number;
+  level: string;
+  questionAmount: number;
+}
+
 const QuizCardList: FC = () => (
   <StyledQuizListContainer>
-    <QuizCard />
-    <QuizCard />
-    <QuizCard />
-    <QuizCard />
-    <QuizCard />
-    <QuizCard />
-    <QuizCard />
-    <QuizCard />
-    <QuizCard />
+    {mockQuizes.map((quiz: Quiz) => (
+      <QuizCard
+        image={quiz.image}
+        title={quiz.name}
+        description={quiz.description}
+        duration={quiz.duration}
+        level={quiz.level}
+        questionAmount={quiz.questionAmount} />
+    ))}
   </StyledQuizListContainer>
 );
 
