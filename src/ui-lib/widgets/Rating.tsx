5f94f8e8fb8e08584d6344ccb5817f1159de3774
Maria Arkhipova <marias1403@yandex.ru>
2023-07-29 01:25:35 +0900

    feat: Achievements

--- src/ui-lib/widgets/Rating.tsx
+++ src/ui-lib/widgets/Rating.tsx
@@ -37,7 +37,7 @@ const Rating: React.FC = () => {
     <StyledDiv
       style={{
         width: '318px',
-        height: '226px',
+        height: '202px',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'flex-start',
