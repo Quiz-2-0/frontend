1d137fc733aafa3af390f4326abcb117235d5a93
Maria Arkhipova <marias1403@yandex.ru>
2023-07-31 16:48:19 +0900

    feat: AchievementPage added

--- src/ui-lib/widgets/Achievements.tsx
+++ src/ui-lib/widgets/Achievements.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from 'react';
+import { useNavigate } from 'react-router';
 import styled from 'styled-components';
 import {
   Title,
@@ -7,12 +8,11 @@ import {
 } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 import StyledDiv from '../StyledDiv';
-import { IconWrapper } from './Achives';
 import { ArrowIcon } from '../icons';
-import achievement1 from '../../images/achievements-img/achievement_img1.png';
-import achievement2 from '../../images/achievements-img/achievement_img2.png';
-import achievement3 from '../../images/achievements-img/achievement_img3.png';
-import achievement4 from '../../images/achievements-img/achievement_img4.png';
+import achieveEasyStart from '../../images/achievements-img/achieve_easy_start.png';
+import achieveSoftKiller from '../../images/achievements-img/achieve_soft_killer.png';
+import achieveSuperSpeed from '../../images/achievements-img/achieve_super_speed.png';
+import achievePermanence from '../../images/achievements-img/achieve_permanence.png';
 
 const AchievementImg = styled.img`
   width: 80px;
@@ -22,45 +22,66 @@ const AchievementImg = styled.img`
   transition: transform 0.5s;
 `;
 
-const Achievements: React.FC = () => (
-  <StyledDiv
-    style={{
-      width: '318px',
-      height: '202px',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'space-between',
-      alignItems: 'flex-start',
-      gap: '40px',
-    }}>
-    <Div
+const StyledNavButton = styled.button`
+  margin: 0;
+  padding: 0;
+  width: 28px;
+  height: 28px;
+  border: none;
+  background-color:rgba(63, 138, 224, 0.05);
+  transition: all ease .3s;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    background-color: rgba(63, 138, 224, 0.15);
+  }
+`;
+
+const Achievements: React.FC = () => {
+  const navigate = useNavigate();
+  return (
+    <StyledDiv
       style={{
-        width: '100%',
-        padding: '0',
+        width: '318px',
+        height: '202px',
         display: 'flex',
-        flexDirection: 'row',
+        flexDirection: 'column',
         justifyContent: 'space-between',
         alignItems: 'flex-start',
+        gap: '40px',
       }}>
-      <Title
-        style={{ textAlign: 'left' }}
-        level='2'>
-        Ачивки
-      </Title>
-      <IconWrapper><ArrowIcon /></IconWrapper>
-    </Div>
-    <Div
-      style={{
-        padding: '0',
-        position: 'relative',
-        height: '80px',
-      }}>
-      <AchievementImg src={achievement1} alt='Ачивка Легкий старт' style={{ left: '0', zIndex: 3 }} />
-      <AchievementImg src={achievement2} alt='Ачивка Легкий старт' style={{ left: '64px', zIndex: 2 }} />
-      <AchievementImg src={achievement3} alt='Ачивка Легкий старт' style={{ left: '128px', zIndex: 1 }} />
-      <AchievementImg src={achievement4} alt='Ачивка Легкий старт' style={{ left: '190px', zIndex: 0 }} />
-    </Div>
-  </StyledDiv>
-);
+      <Div
+        style={{
+          width: '100%',
+          padding: '0',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'flex-start',
+        }}>
+        <Title
+          style={{ textAlign: 'left' }}
+          level='2'>
+          Ачивки
+        </Title>
+        <StyledNavButton type='button' onClick={() => navigate('/achievements')}><ArrowIcon /></StyledNavButton>
+      </Div>
+      <Div
+        style={{
+          padding: '0',
+          position: 'relative',
+          height: '80px',
+        }}>
+        <AchievementImg src={achieveEasyStart} alt='Ачивка Легкий старт' style={{ left: '0', zIndex: 3 }} />
+        <AchievementImg src={achieveSoftKiller} alt='Ачивка Софт киллер' style={{ left: '64px', zIndex: 2 }} />
+        <AchievementImg src={achieveSuperSpeed} alt='Ачивка Супер скорость' style={{ left: '128px', zIndex: 1 }} />
+        <AchievementImg src={achievePermanence} alt='Ачивка Постоянство' style={{ left: '190px', zIndex: 0 }} />
+      </Div>
+    </StyledDiv>
+  );
+};
 
 export default Achievements;
