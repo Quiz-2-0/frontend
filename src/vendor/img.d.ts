1934a95b492280272aa34013d2c2a93a4d2fd481
Ivan Gavrilov <julivgan@gmail.com>
2023-08-08 21:44:45 +0400

    feat: #62 update ts-types for images

--- src/vendor/img.d.ts
+++ src/vendor/img.d.ts
@@ -1,3 +1,15 @@
-declare module '*.jpg';
-declare module '*.png';
-declare module '*.svg';
+declare module '*.jpg' {
+  const value: never;
+  export = value;
+}
+
+declare module '*.png' {
+  const value: never;
+  export = value;
+}
+
+declare module '*.svg' {
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
